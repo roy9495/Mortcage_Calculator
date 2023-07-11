cd8bf3b0e5c8362c2911a741cc6501dbf27a835a
TATHAGATA ROY <royzen9495@gmail.com>
2023-07-11 16:05:18 +0000

    Update 5 files

---
+++ src/components/Menu/Button.js
@@ -0,0 +1,55 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Tooltip from "@mui/material/Tooltip";
+
+export default function BasicMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button
+        id="basic-button"
+        aria-controls={open ? "basic-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+      >
+        <Tooltip title="Menu" arrow>
+          <IconButton
+            size="large"
+            edge="start"
+            color="default"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Tooltip>
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <MenuItem onClick={handleClose}>Save Data</MenuItem>
+        <MenuItem onClick={handleClose}>Download Data</MenuItem>
+        <MenuItem onClick={handleClose}>Send Email</MenuItem>
+      </Menu>
+    </div>
+  );
+}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,10 +5,16 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
+import BasicMenu from "./Menu/Button";
 import { Container } from "@mui/material";
+import Tooltip from "@mui/material/Tooltip";
+import LoginIcon from "@mui/icons-material/Login";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 const Navbar = () => {
+  const handleClick = () => {
+    window.location.href = "https://github.com/roy9495/Mortgage-Calculator";
+  };
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -20,14 +26,23 @@ const Navbar = () => {
             aria-label="menu"
             sx={{ mr: 2 }}
           >
-            <MenuIcon />
+            <BasicMenu />
           </IconButton>
           <Container maxWidth="md">
             <Typography variant="h5" component="div" sx={{ flexGrow: 2 }}>
               Mortgage Calculator
             </Typography>
           </Container>
-          <Button color="inherit">Login</Button>
+          <Tooltip title="Github" arrow>
+            <Button onClick={handleClick}>
+              <GitHubIcon fontSize="medium" color="action" />
+            </Button>
+          </Tooltip>
+          <Tooltip title="Login" arrow>
+            <Button color="inherit" >
+              <LoginIcon />
+            </Button>
+          </Tooltip>
         </Toolbar>
       </AppBar>
     </Box>
